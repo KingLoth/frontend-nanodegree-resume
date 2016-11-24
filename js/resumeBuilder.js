2486a758765c2eb904c85d63f86176e404d682aa
locoge xavier <locoge xavier>
2016-11-24 22:01:09 +0100

    suppression de hello world

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,5 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var firstName = "Xavier";
+var age = 25;
+var awsomeThoughts = "I am " + firstName + " and I am AWESOME";
+
+console.log(awsomeThoughts);
