814b44dcef5c3dd5058653a94549c63bb2248a84
locoge xavier <locoge xavier>
2016-11-26 17:36:16 +0100

    premeir objet

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,13 +1,22 @@
-var firstName = "Xavier";
-var lastName = "Locoge"
-var actualRole = "Ingénieur étude et developpement"
-var age = 25;
-var skills = ["programation", "développement Front-End", "Scrum Master"]
+var bio = {
+  "name" : "Xavier Locoge",
+  "role" : "Ingénieur étude et developpement",
+  "contacts" : {
+    "mobile" : "0688637110",
+    "email" : "xavier.locoge@icloud.com",
+    "github" : "king_loth",
+    "twitter" : "@KingLoth",
+    "location" : "Toulouse"
+  },
+  "welcomMessage" : "lorem ipsum dolor sit amet etc etc etc...",
+  "skills" :  ["programation", "développement Front-End", "Scrum Master"],
+  "bioPic" : "images/fry.jpg"
+};
 
 
-var formatedName = HTMLheaderName.replace("%data%", firstName + " " + lastName);
-var formatedRole = HTMLheaderRole.replace("%data%", actualRole);
+var formatedName = HTMLheaderName.replace("%data%", bio.name);
+var formatedRole = HTMLheaderRole.replace("%data%", bio.role);
 
 $("#header").prepend(formatedRole);
 $("#header").prepend(formatedName);
-$("#main").append(skills);
+$("#main").append(bio.skills);
