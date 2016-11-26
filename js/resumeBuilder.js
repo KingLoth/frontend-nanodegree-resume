67a7a763068bffeaf65efcae3098a15dddf3a664
locoge xavier <locoge xavier>
2016-11-26 17:02:59 +0100

    premiere utilisation des tableau

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,6 +2,7 @@ var firstName = "Xavier";
 var lastName = "Locoge"
 var actualRole = "Ingénieur étude et developpement"
 var age = 25;
+var skills = ["programation", "développement Front-End", "Scrum Master"]
 
 
 var formatedName = HTMLheaderName.replace("%data%", firstName + " " + lastName);
@@ -9,3 +10,4 @@ var formatedRole = HTMLheaderRole.replace("%data%", actualRole);
 
 $("#header").prepend(formatedRole);
 $("#header").prepend(formatedName);
+$("#main").append(skills);
