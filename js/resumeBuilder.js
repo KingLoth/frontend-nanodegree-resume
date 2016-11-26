0fe6e7b4b492cb658ebe64766cb9278d12daf1e6
locoge xavier <locoge xavier>
2016-11-26 16:23:47 +0100

    utilisation de append et de prepend

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,11 @@
 var firstName = "Xavier";
 var lastName = "Locoge"
+var actualRole = "Ingénieur étude et developpement"
 var age = 25;
-var formatedName = HTMLheaderName.replace("%data%", firstName + " " + lastName);
 
-$("#header").append(formatedName);
+
+var formatedName = HTMLheaderName.replace("%data%", firstName + " " + lastName);
+var formatedRole = HTMLheaderRole.replace("%data%", actualRole);
+
+$("#header").prepend(formatedRole);
+$("#header").prepend(formatedName);
